02d1bd7f12b614676c3311825c2f90f18a9d5447
しろ <83211829+naychan-v@users.noreply.github.com>
2021-12-13 10:52:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ global.tum = fs.readFileSync('./src/Ah5.jpeg')
 global.tum2 = fs.readFileSync('./src/emror.jpeg')
 global.thumb = 'https://i.ibb.co/34PV5yH/By-Relldev.jpg'
 global.thumb2 = fs.readFileSync('./src/emror.jpeg')
-global.owner = ['6281379753850', '6283820073017', '6283801499848','62813828362494','85290882526','6285240750713','6282125986924','6288286421519'] // Put your number here
+global.owner = ['6282134729981'] // Put your number here
 global.mods = ['6281379753850'] // Want some help?
-global.prems = ['6281379753850','6285893120588','6285656615441'] // Premium user has unlimited limit
+global.prems = ['6282134729981'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
